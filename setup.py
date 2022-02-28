326700bfc36bb68d5ad76956d233590b94ee1fcf
PrashanthaTP <tpprashantha@gmail.com>
2022-02-28 22:19:01 +0530

    Pack(setup): update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 def read_file(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "gbpacman",
-    version = "0.0.1",
-    author = "Prashantha TP",
-    description = ("Package Manager for Git Bash"),
-    license = "MIT",
-    keywords = "msys2 gitbash bash pacman",
-    url = "https://github.com/PrashanthaTP/gbpacman",
-    #packages=['an_example_pypi_project', 'tests'],
+    name="gbpacman",
+    version="0.0.1",
+    author="Prashantha TP",
+    description=("Package Manager for Git Bash"),
+    license="MIT",
+    keywords="msys2 gitbash bash pacman",
+    url="https://github.com/PrashanthaTP/gbpacman",
+    packages=find_packages('gbpacman', exclude=['tests']),
     long_description=read_file('README.md'),
 )
