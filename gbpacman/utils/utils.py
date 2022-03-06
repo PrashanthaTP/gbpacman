341365dde683c427228c622e0d063e45b43be19f
PrashanthaTP <tpprashantha@gmail.com>
2022-03-06 11:41:53 +0530

    Util(*): add func to display attributes

--- gbpacman/utils/utils.py
+++ gbpacman/utils/utils.py
@@ -1,7 +1,7 @@
 import json
 from plogger import get_logger
 
-logger = get_logger(__file__.split(".")[0])
+logger = get_logger(__name__)
 
 
 def load_settings(json_filepath):
@@ -11,3 +11,8 @@ def load_settings(json_filepath):
     except FileNotFoundError as error:
         logger.error(error)
         return {}
+
+
+def print_attributes(obj):
+    for key, value in vars(obj).items():
+        logger.info("%s : %s" % (key, value))
