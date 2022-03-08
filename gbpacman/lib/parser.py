c5af0e2e8136d554ad45555d79381793763fa19d
PrashanthaTP <tpprashantha@gmail.com>
2022-03-08 23:23:56 +0530

    Feat(parser): add function related to package list
    - get_columns : get a list of strings of tags of given type
    - get_packages_list : gets a list of packages from given table tag
    - get_package_info_page_link : get the tag containing link to the
                                  package download page

--- gbpacman/lib/parser.py
+++ gbpacman/lib/parser.py
@@ -1,6 +1,13 @@
+from collections import namedtuple
 from bs4 import BeautifulSoup
+from re import (search as re_search,
+                IGNORECASE as re_IGNORECASE)
 from gbpacman.utils.logger import get_global_logger
 logger = get_global_logger()
+
+
+PackageInfo = namedtuple(
+    "PackageInfo", ["name", "description", "link", "version"])
 
 
 def parse_html_response(response, parser="html.parser"):
@@ -31,7 +38,7 @@ def extract(response, filter_fn=None, *args, **kwargs):
 
 def extract_package_table(parent_tag):
     title_tag, table_container_tag = get_direct_children(parent_tag)
-    #title_text = title_tag.get_text(strip=True)
+    # title_text = title_tag.get_text(strip=True)
     table_tag = table_container_tag.table
 
     return title_tag, table_tag
@@ -39,3 +46,46 @@ def extract_package_table(parent_tag):
 
 def get_children_of_type(tag, **attrs):
     return tag.find_all(**attrs)
+
+
+def get_columns(row_tag, **attrs):
+    cols = get_children_of_type(row_tag, **attrs)
+    return [col.get_text() for col in cols]
+
+
+def get_packages_list(table_tag):
+
+    # table_row_headings = get_children_of_type(table_tag.thead,
+    #                                          **{"name": "th"})
+    # package_rows = [[row_heading.get_text()
+    #                 for row_heading in table_row_headings]]
+    package_rows = []
+
+    for row in get_children_of_type(table_tag.tbody,
+                                    **{"name": "tr"}):
+        name_tag, version, description = get_children_of_type(row,
+                                                              **{"name": "td"})
+        link = name_tag.a["href"]
+        package_rows.append(PackageInfo(name_tag.get_text(strip=True),
+                                        description.get_text(strip=True),
+                                        link,
+                                        version.get_text(strip=True)
+                                        )
+                            )
+    return package_rows
+
+
+def get_package_info_page_link(package_page):
+    def is_base_package_link(tag):
+        prev_sibling = tag.find_previous_sibling()
+        if prev_sibling is None:
+            return False
+        prev_heading = re_search(r".*Binary Packages\s?:?\s?.*",
+                                 prev_sibling.get_text(),
+                                 re_IGNORECASE)
+        return tag.name == "dd"  \
+            and prev_sibling.name == "dt"\
+            and prev_heading is not None
+
+    soup = parse_html_response(package_page)
+    return soup.find(is_base_package_link)
