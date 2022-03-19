26305dfae4770da9ea285d34af7188215154ccab
PrashanthaTP <tpprashantha@gmail.com>
2022-03-19 15:11:53 +0530

    Chore(calls): add response code check

--- gbpacman/lib/calls.py
+++ gbpacman/lib/calls.py
@@ -3,6 +3,8 @@ from requests import get as req_get
 from gbpacman.utils.logger import get_global_logger
 logger = get_global_logger()
 
+SUCCESS_CODE = 200
+
 
 def ping_url(url: str):
     """Performs a get request to given url and returns a response object
@@ -15,6 +17,10 @@ def ping_url(url: str):
     """
     #logger.debug("pinging url %s"%url)
     return req_get(url)
+
+
+def check_response(response):
+    assert(response.status_code == SUCCESS_CODE)
 
 
 def get_packages_list():
