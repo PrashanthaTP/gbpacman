e5b8ff1e2c2dcecb5f3d69d6a5eb0e2bc01a8ae0
PrashanthaTP <tpprashantha@gmail.com>
2022-03-19 15:14:20 +0530

    Tests(setup): fix zstd exe related tests

--- gbpacman/tests/test_setup_package.py
+++ gbpacman/tests/test_setup_package.py
@@ -3,6 +3,7 @@ import unittest
 from plogger import get_logger
 
 from gbpacman.lib import setup_package
+from gbpacman import settings
 
 url = "https://packages.msys2.org"
 
@@ -12,21 +13,23 @@ logger = get_logger(__name__)
 class TestSetup(unittest.TestCase):
     def setUp(self):
         super().__init__()
-        self.out_dir = "out"
+        self.bin_dir = os.path.join(settings["BASE_DIR"], "bin")
         #self.zstd_filename = "zstd-1.5.2.tar.gz"
-        self.zstd_filename = "zstd-1.5.2-win64.zip"
+        self.zstd_folder_name = os.path.basename(
+            settings["exe"]["zstd_url_windows"])
 
     def test_download_zstd(self):
 
-        zstd_fullpath = os.path.join(self.out_dir, self.zstd_filename)
+        zstd_fullpath = os.path.join(self.bin_dir, self.zstd_folder_name)
         setup_package.download_zstd()
         self.assertTrue(os.path.isfile(zstd_fullpath))
 
     def test_unzip_zstd(self):
-        zstd_fullpath = os.path.join(
-            self.out_dir, self.zstd_filename.strip("zip"))
+        zstd_exe = os.path.join(self.bin_dir,
+                                self.zstd_folder_name.rstrip(".zip"),
+                                "zstd.exe")
         setup_package.unzip_zstd()
-        self.assertTrue(os.path.isfile(zstd_fullpath))
+        self.assertTrue(os.path.isfile(zstd_exe), f"Needed {zstd_exe}")
 
 
 if __name__ == "__main__":
