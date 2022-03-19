09c519695341ae634d71f9e68424aafe7909ab74
PrashanthaTP <tpprashantha@gmail.com>
2022-03-19 23:50:09 +0530

    Chore(ui): add seperators while displaying

--- gbpacman/lib/ui.py
+++ gbpacman/lib/ui.py
@@ -6,8 +6,9 @@ logger = get_global_logger()
 def display_packages(packages: list):
     if len(packages) == 0:
         logger.warning("No pakcages to display")
-    print("-"*30)
     for package in packages:
+        print("-"*30)
         print(f"Name    : {package.name}")
         print(f"Version : {package.version}")
         print(f"About   : {package.description}")
+    print("-"*30)
