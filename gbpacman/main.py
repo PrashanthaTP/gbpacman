0243d6ab715a85c930957dcb7ce8f013d7d363e2
PrashanthaTP <tpprashantha@gmail.com>
2022-03-20 00:00:13 +0530

    Feat(pack): add package listing feat

--- gbpacman/main.py
+++ gbpacman/main.py
@@ -1,19 +1,49 @@
 from os import path as os_path
-from plogger import get_logger
 
-from gbpacman.utils import get_cmd_options
-from gbpacman.utils import load_settings
+from gbpacman.utils import (get_cmd_options,
+                            load_settings,
+                            get_global_logger)
 
-logger = get_logger(os_path.basename(__file__))
-logger.debug("Hello")
+from gbpacman.lib import (calls,
+                          parser,
+                          filters,
+                          api,
+                          ui)
+from gbpacman.config import settings
 
-ROOT_DIR=os_path.dirname(os_path.abspath(__file__))
-SETTINGS_FILE="settings.json"
+logger = get_global_logger()
+
+SETTINGS_FILE = "settings.json"
+BIN_DIR = os_path.join(settings["BASE_DIR"], "bin")
+MSYS_URL = settings["packages_url"]
 
 
+def get_response(query):
+    url = f"{MSYS_URL}/search?q={query}"
+    logger.debug(f"Pinging...{url}")
+    response = calls.ping_url(url)
+    return response
+
+
+def get_list_of_packages(package_name):
+    response = get_response(query=package_name)
+    calls.check_response(response)
+    target_tag = parser.extract(response,
+                                filters.is_search_table,
+                                attrs={"class": "card mb-3"},
+                                limit=1)[0]
+    _, table = parser.get_package_table(target_tag)
+    packages_list = parser.get_packages_list(table)
+    return packages_list
+
+
+def list_matching_packages(package_name):
+    packages = get_list_of_packages(package_name)
+    ui.display_packages(packages)
 
 
 def main():
-    get_cmd_options()
-    settings = load_settings(os_path.join(ROOT_DIR,SETTINGS_FILE))
-    print(settings)
+    options = get_cmd_options()
+    package_name = options.list
+    if package_name:
+        list_matching_packages(package_name)
--- gbpacman/utils/cmdline.py
+++ gbpacman/utils/cmdline.py
@@ -4,8 +4,12 @@ import argparse
 def get_cmd_options():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Git Bash Package Manager")
+    parser.add_argument('-l', '--list', type=str,
+                        help="list packages with given name")
     parser.add_argument('-i', '--install', type=str,
                         help="install package from msys package inventory")
+    parser.add_argument('-u', '--install-from-url', type=str,
+                        help="install package given zip file url")
     parser.add_argument('--uninstall', type=str,
                         help="uninstall the package from the system if it exists")
     options = parser.parse_args()
