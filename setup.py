e74ece161dc089db1d07c8059c53c5cbaaf3b8e9
PrashanthaTP <tpprashantha@gmail.com>
2022-03-20 12:16:20 +0530

    Exp(setup): require plogger

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ def read_file(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+install_requires = ['plogger']
+dependency_links = [
+    "plogger @ git+https://github.com/PrashanthaTP/plogger.git@main#egg=plogger-0.0.1"]
 setup(
     name="gbpacman",
     version="0.0.1",
@@ -17,6 +20,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     package_dir={"gbpacman": "gbpacman"},
     include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     long_description=read_file('README.md'),
     entry_points={
         'console_scripts': ['gbpacman=gbpacman.main:main']
