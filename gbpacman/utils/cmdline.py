3ca3dcc9faed437da4901c01350d864185032c17
PrashanthaTP <tpprashantha@gmail.com>
2022-03-20 14:44:14 +0530

    Chore(args): comment out option to pass url

--- gbpacman/utils/cmdline.py
+++ gbpacman/utils/cmdline.py
@@ -8,8 +8,8 @@ def get_cmd_options():
                         help="list packages with given name")
     parser.add_argument('-i', '--install', type=str,
                         help="install package from msys package inventory")
-    parser.add_argument('-u', '--install-from-url', type=str,
-                        help="install package given zip file url")
+    #parser.add_argument('-u', '--install-from-url', type=str,
+    #                    help="install package given zip file url")
     parser.add_argument('--uninstall', type=str,
                         help="uninstall the package from the system if it exists")
     options = parser.parse_args()
