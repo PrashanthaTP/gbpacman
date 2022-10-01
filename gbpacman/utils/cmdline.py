8efa06c3b95308174e1b762322c7252affe60f6d
PrashanthaTP <tpprashantha@gmail.com>
2022-10-01 23:01:33 +0530

    Feat(ui): add version info in help description

--- gbpacman/utils/cmdline.py
+++ gbpacman/utils/cmdline.py
@@ -1,14 +1,14 @@
 import argparse
 
 
-def get_cmd_options():
+def get_cmd_options(settings):
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Git Bash Package Manager")
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=f"Git Bash Package Manager ({settings['VERSION']})")
     parser.add_argument('-l', '--list', type=str,
                         help="list packages with given name")
     parser.add_argument('-i', '--install', type=str,
                         help="install package from msys package inventory")
-    #parser.add_argument('-u', '--install-from-url', type=str,
+    # parser.add_argument('-u', '--install-from-url', type=str,
     #                    help="install package given zip file url")
     parser.add_argument('--uninstall', type=str,
                         help="uninstall the package from the system if it exists")
